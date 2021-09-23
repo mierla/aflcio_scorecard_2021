f26861df7a3caab73f67ffe7fd3b4719a0f84a62
mierla <laurenmier@gmail.com>
2021-09-23 14:57:16 -0400

    Update Legislators.js
    Add 2021 scorecard link to landing page

--- src/components/Legislators.js
+++ src/components/Legislators.js
@@ -31,6 +31,9 @@ export default withStore(function Legislators(props) {
               Click <a href="/all-scores">here</a> to see them all.
             </h1>
             <h1 className="zero-state-message">
+              <a href="/pdf/2021-Working-Families-Scorecard.pdf">Download the 2021 Scorecard [PDF]</a>           
+            </h1>
+            <h1 className="zero-state-message">
               Can't find what you're looking for? Check out <a href="http://groundgamer.org" target="_blank" rel="noopener noreferrer">GroundGamer.org</a>.
             </h1>
           </div>
