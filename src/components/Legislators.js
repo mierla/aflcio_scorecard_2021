77316a1cde6eedc7654105a61bda886e781e2ddf
mierla <laurenmier@gmail.com>
2021-09-23 16:49:22 -0400

    Update Legislators.js
    import the pdf link the right way, I guess, if I have to.

--- src/components/Legislators.js
+++ src/components/Legislators.js
@@ -2,6 +2,7 @@ import React from 'react'
 import FindMyLegislators from './FindMyLegislators'
 import LegislatorCard from './LegislatorCard'
 import withStore from '../services/legislators-store'
+import twentyTwentyone from '../assets/pdfs/2021-Maine-AFLCIO-Scorecard.pdf'
 import thumbnail from '../assets/images/2021-scorecard-cover.png'
 
 
@@ -32,9 +33,9 @@ export default withStore(function Legislators(props) {
               Click <a href="/all-scores">here</a> to see them all.
             </h1>
             <h1 className="zero-state-message">
-              <a href="/pdf/2021-Maine-AFLCIO-scorecard.pdf">Download the 2021 Scorecard [PDF]</a>           
+              <a href={twentyTwentyone}>Download the 2021 Scorecard [PDF]</a>           
             </h1>
-            <a href="/pdf/2021-Maine-AFLCIO-scorecard.pdf">
+            <a href={twentyTwentyone}>
               <img src={thumbnail} alt="2021 Scorecard Thumbnail" class="homepage-thumb" />
             </a>  
             <h1 className="zero-state-message">
